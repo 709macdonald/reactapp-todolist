26aebf9295247c081cfc52e92ff611cf3e7f57e8
709macdonald <709macdonald@gmail.com>
2024-08-15 21:59:02 -0230

    Importing components to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
+
 function App() {
-  return;
-  <></>;
+  return (
+    <main>
+      <TodoInput />
+      <TodoList />
+    </main>
+  );
 }
 
 export default App;
