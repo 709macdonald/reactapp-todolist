f0750af8537d78c8b0936b3b6d8aea36da6697a5
709macdonald <709macdonald@gmail.com>
2024-08-17 20:16:00 -0230

    Adding new todos

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function TodoInput() {
+export default function TodoInput(props) {
+  const { handleAddTodos } = props;
+  const [todoValue, setTodoValue] = useState("");
+
   return (
     <header>
-      <input placeholder="Enter Todo..." />
-      <button>Add</button>
+      <input
+        value={todoValue}
+        onChange={(e) => {
+          setTodoValue(e.target.value);
+        }}
+        placeholder="Enter Todo..."
+      />
+      <button
+        onClick={() => {
+          handleAddTodos(todoValue);
+        }}
+      >
+        Add
+      </button>
     </header>
   );
 }
