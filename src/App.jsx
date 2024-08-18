c47efb18f015c785318b2133835e9e7c2a1f4023
709macdonald <709macdonald@gmail.com>
2024-08-18 15:57:58 -0230

    Edit button and local storage work

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
 
@@ -6,8 +6,13 @@ function App() {
   const [todos, setTodos] = useState([]);
   const [todoValue, setTodoValue] = useState("");
 
+  function persistData(newList) {
+    localStorage.setItem("todos", JSON.stringify({ todos: newList }));
+  }
+
   function handleAddTodos(newTodo) {
     const newTodoList = [...todos, newTodo];
+    persistData(newTodoList);
     setTodos(newTodoList);
   }
 
@@ -15,14 +20,29 @@ function App() {
     const newTodoList = todos.filter((todo, todoIndex) => {
       return todoIndex !== index;
     });
+    persistData(newTodoList);
     setTodos(newTodoList);
   }
 
-  function handleEditToo(index) {
+  function handleEditTodo(index) {
     const valueToBeEdited = todos[index];
     setTodoValue(valueToBeEdited);
     handleDeleteTodo(index);
   }
+
+  useEffect(() => {
+    if (!localStorage) {
+      return;
+    }
+
+    let localTodos = localStorage.getItem("todos");
+    if (!localTodos) {
+      return;
+    }
+
+    localTodos = JSON.parse(localTodos).todos;
+    setTodos(localTodos);
+  }, []);
 
   return (
     <>
@@ -32,7 +52,7 @@ function App() {
         handleAddTodos={handleAddTodos}
       />
       <TodoList
-        handleEditToo={handleEditToo}
+        handleEditTodo={handleEditTodo}
         handleDeleteTodo={handleDeleteTodo}
         todos={todos}
       />
